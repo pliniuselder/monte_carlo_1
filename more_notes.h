5929855223e353fa56cc28f5426b069e67e0b3b3
pliniuselder <79376497+pliniuselder@users.noreply.github.com>
2021-08-15 07:39:17 +0900

    Update more_notes.h

--- more_notes.h
+++ more_notes.h
@@ -1,1 +1,69 @@
-
+Atelier Firis The Alchemist and the Mysterious Journey 2017
+Xenoblade 2010
+Xenoblade X 2015
+Xenoblade 2 2017
+BLUE REFLECTION 2017
+Rance X 2018
+OCTOPATH TRAVELER 2018
+GRANBLUE FANTASY 2014
+ARMORED CORE VERDICT DAY 2013
+Pokémon Super Mystery Dungeon 2015
+Kirby Star 2018
+BRAVELY DEFAULT 2012-2013
+PERSONA 3 4 5 (2006-2008-2016)
+Splatoon 2 2017
+Ys VIII: Lacrimosa of Dana 2017
+Caligula Overdose 2018
+LORD of VERMILION 2011
+Danganronpa V3 2017
+Fire Emblem 2017
+The Legend of Heroes: Trails of Cold Steel 2013
+megido72 2017
+Monster Hunter: World 2018
+Ar nosurge: Ode to an Unborn Star 2014
+Ciel nosurge 2012
+NieR Replicant 2010
+Kantai Collection 2013
+SUPER MARIO ODYSSEY 2017
+Romancing Sa・Ga3 1995 remaster 2019
+TRIALS of MANA 2017
+Tales of Berseria 2016
+Wonderland Wars 2015
+Fate/Grand Order 2015
+7th DRAGON III code:VFD 2015
+Lost Odyssey 2008
+Etrian Odyssey Nexus 2019
+'''
+SaGa Frontier 1997-1999 remaster 2021
+Rance 1989-2018
+Atelier 1997-2020
+Ar tonelico 2006-2010
+Chrono Cross 1999
+Chrono Trigger 1995
+ACE COMBAT 1995-2019
+DARIUS 1987-2016
+Ys 1987-2019
+Fire Emblem 1990-2019
+Megami Tensei 1987-2012
+The Legend of Heroes 1989-2020
+WILD ARMS 1996-2018
+Romancing Sa・Ga 1992-1995
+Seiken Densetsu 1991-2021
+Tales 1995-2021
+Mother 1989-2006
+Danganronpa 2010-2017
+Kirby 1992-2020
+The Legend of Zelda 1986-2021
+GURUMIN 2004
+Suikoden 1995-2012
+Sakura Wars 1996-2019
+7th DRAGON 2009-2015
+.hack 2002-2005
+.hack//G.U. 2006-2017
+.hack//Link 2010
+SONIC 1991-2019
+Devil May Cry 2001-2019
+Golden Sun 2001-2010
+Etrian Odyssey 2007-2017
+KINGDOM HEARTS 2002-2020
+'''
